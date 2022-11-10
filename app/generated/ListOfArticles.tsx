6580e3963c9df83e023a37d9f56d49e2e411714e
David Johnston <djohnston@cydarm.com>
2022-11-11 10:36:40 +1300

    Move blog posts. Add articles generation script.

---
+++ app/generated/ListOfArticles.tsx
@@ -0,0 +1,65 @@
+
+        export const ListOfArticles = () => {
+
+
+
+            return <ul>
+                <li>
+                <a
+                    href="/posts/agnostic_state_managment_in_react"
+                >
+                    agnostic_state_managment_in_react        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/blog ideas"
+                >
+                    blog ideas        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/dependency_injecting_when_using_redux"
+                >
+                    dependency_injecting_when_using_redux        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/different_approaches_to_form_element_state_in_react"
+                >
+                    different_approaches_to_form_element_state_in_react        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/extensible_changes"
+                >
+                    extensible_changes        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/how_to_effectively_hire_contractors"
+                >
+                    how_to_effectively_hire_contractors        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/the_case_for_splitting_tests_into_multiple_it_blocks"
+                >
+                    the_case_for_splitting_tests_into_multiple_it_blocks        </a>
+            </li>
+                
+                <li>
+                <a
+                    href="/posts/whats_the_role_of_qa"
+                >
+                    whats_the_role_of_qa        </a>
+            </li>
+                
+        </ul>
+    }
+    
