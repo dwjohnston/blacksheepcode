9b85b1ace12bf12edbe1342d81f88cdb9f379435
David Johnston <djohnston@cydarm.com>
2022-11-11 10:41:18 +1300

    Remove old stuff

--- projects/agnostic-state-management/src/components/part3/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ServicesProvider } from "./ServicesProvider"
-import { fetchAllTodos } from "./services/TodosService"
-import { TodoList } from "./TodoList"
-
-export const App = () => {
-    return <ServicesProvider fetchTodos={fetchAllTodos}>
-        <TodoList />
-    </ServicesProvider>
-}
--- src/blog ideas.md
+++
@@ -1,8 +0,0 @@
-# Bite Sized: 
-
-- What works for one organisation, doesn't neceserily work for your organisation
-- Good code is easy to delete. 
-- Accessible selectors for radio buttons/checkboxes
-- Why I don't like enums in TypeScript. 
-
-
