a78e8b6718ec8e78a96d66ea699221222a1a4d26
David Johnston <djohnston@cydarm.com>
2022-11-11 11:18:55 +1300

    Add styles. Update main page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,8 +2,44 @@ import { ListOfArticles } from "~/generated/ListOfArticles";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <ListOfArticles/>
-    </div>
+
+    <>
+      <div className="main">
+
+        <h1>Black Sheep Code</h1>
+        <p>Personal Website of David Johnston</p>
+
+        <div>
+          <h2>Work</h2>
+          <ul>
+            <li><a href="https://github.com/dwjohnston" target="_blank">Github</a></li>
+            <li><a href="https://stackoverflow.com/users/1068446/dwjohnston" target="_blank">Stack Overflow</a> </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>Art</h2>
+          <ul>
+            <li><a href="http://new.geoplanets.io/" target="_blank">GeoPlanets</a> - My first foray in to geometric art.
+            </li>
+            <li><a href="https://tinymassive.herokuapp.com/" target="_blank">Rider on the Waves</a> - A submission for
+              the Tiny Massive light show at the
+              Reykavik Lights Festival, Iceland. (Hosted on Heroku, I'm suprised it's still running)</li>
+
+          </ul>
+        </div>
+
+
+      </div>
+
+      <div className ="main">
+
+        <h2>Blog</h2>
+        <ListOfArticles />
+      </div>
+
+      <p className ="open-source">I support open source: <a href="https://opencollective.com/blacksheepcode" target="_blank">Open Collective</a>
+      </p>
+    </>
   );
 }
