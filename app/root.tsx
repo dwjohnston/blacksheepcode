7ed02e10a36b230019d36120eb2d7b8bfbc3aba5
David Johnston <david@blacksheepcode.com>
2023-04-14 17:36:38 +1000

    Update image tag (#44)

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Black Sheep Code - The Personal Blog of David Johnston",
   viewport: "width=device-width,initial-scale=1",
-  'og:image': bscImage
+  'og:image': `https://blacksheepcode.com/${bscImage}`
 });
 
 
