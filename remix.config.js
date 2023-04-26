6e0326c767c8e36b3a8b50346c693036ac91d885
David Johnston <david@blacksheepcode.com>
2023-04-26 15:24:38 +1000

    Contract testing 2 (#46)
    * Several posts re: contract testing
    
    Finalise posts
    
    Redo screenshots
    
    * Remove that unused file
    
    * Improve drafts experience
    
    * Promote post to published

--- remix.config.js
+++ remix.config.js
@@ -1,22 +1,23 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+
 module.exports = {
   serverBuildTarget: "netlify",
   server:
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? "./server.js"
       : undefined,
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ["**/.*", ...(process.env.NODE_ENV === 'development' ? [] : ["drafts/*"])],
   mdx: async (filename) => {
 
     const [rehypeHighlight] = await Promise.all([
       import("rehype-highlight").then((mod) => mod.default),
-      
+
     ]);
 
     return {
       rehypePlugins: [rehypeHighlight]
     }
-  }, 
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: ".netlify/functions-internal/server.js",
