c8bdaf93913519870412c14e16cae6b734a81eee
David Johnston <david@blacksheepcode.com>
2023-06-13 16:11:40 +1000

    Try this. (#50)

---
+++ utils/getSocialMetas.ts
@@ -0,0 +1,34 @@
+
+
+import bscImage from "./assets/blacksheep_fb_wide.webp";
+
+
+export function getSocialMetas({
+    url,
+    title = 'Helping people make the world a better place through quality software',
+    description = 'Make the world better with software',
+    image = bscImage,
+    keywords = '',
+  }: {
+    image?: string
+    url: string
+    title?: string
+    description?: string
+    keywords?: string
+  }) {
+    return {
+      title,
+      description,
+      keywords,
+      image,
+      'og:url': url,
+      'og:title': title,
+      'og:description': description,
+      'og:image': image,
+      'twitter:card': image ? 'summary_large_image' : 'summary',
+      'twitter:title': title,
+      'twitter:description': description,
+      'twitter:image': image,
+      'twitter:image:alt': title,
+    }
+  }
