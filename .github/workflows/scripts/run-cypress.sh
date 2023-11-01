87ea63b55dadc839d5aaa0e151b5c25845451bc8
David Johnston <david@blacksheepcode.com>
2023-11-02 09:17:23 +1100

    :|

--- .github/workflows/scripts/run-cypress.sh
+++ .github/workflows/scripts/run-cypress.sh
@@ -10,6 +10,6 @@ if [ $exit_code -ne 0 ]; then
     exit $exit_code
 fi
 
-deploy_url=$(echo "$output" | grep -o 'Deploy URL: http://[^ ]*' | awk '{print $3}')
-echo $deploy_url
+deploy_url=$(echo "$output" | grep -o 'Deploy URL: https://[^ ]*' | awk '{print $3}')
+echo "Deploy URL is: $deploy_url"
 CYPRESS_BASE_URL=$deploy_url yarn test:cypress:ci
