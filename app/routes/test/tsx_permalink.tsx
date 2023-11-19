ca5853a11750b25873bbcff7105218a7f70ff5cb
David Johnston <david@blacksheepcode.com>
2023-11-19 14:27:45 +1100

    Add post about msw bundler (#82)
    * Implements the full front matter solution.
    
    * Update build command.
    
    * test command
    
    * Regen package lock
    
    * Park changes
    
    * Update configuring remix app with MSW bundler.
    
    * Regen package lock.

---
+++ app/routes/test/tsx_permalink.tsx
@@ -0,0 +1,11 @@
+import { GithubPermalink, GithubPermalinkProvider} from "react-github-permalink"
+
+
+export default function() {
+    return <div>I am a .tsx file
+
+<GithubPermalinkProvider githubToken="foo">
+<GithubPermalink permalink="https://github.com/dwjohnston/react-github-permalink/blob/5b15aa07e60af4e317086f391b28cadf9aae8e1b/sample_files/sample1.go#L1-L5"/>
+</GithubPermalinkProvider>
+    </div>
+}
