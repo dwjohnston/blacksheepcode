e43b51738880cc92355f740efdea4b0e2f71141e
David Johnston <david@blacksheepcode.com>
2024-03-20 16:36:53 +1100

    Rss2 (#103)
    * Add sitemaps, robots.txt
    
    * Add rss.xml
    
    * Fix sitemap/rss solutioon.
    
    * Fixes the RSS + Sitemap solution - of course I can't use fs... /facepalm.
    
    * Update RSS formatting, add image.

--- utils/getRss.ts
+++ utils/getRss.ts
@@ -9,11 +9,16 @@ const metadataArray = Object.values(allMetadata);
     xml+=`<description>Modern web development - testability, extensibility, declarative APIs, declarative code from open specs and more.</description>`
     xml+=`<language>en-AU</language>`
     xml+=`<link>https://blacksheepcode.com</link>`
+    xml+=`<image>
+    <url>https://blacksheepcode.com/blacksheep_fb_wide.webp</url>
+  </image>
+    `
     metadataArray.forEach((json) => {
 
             xml += `  <item>\n`;
-            xml += `    <title>${rootUrl}/${json.frontmatter.meta.title}</title>\n`;
-            xml += `    <description>${rootUrl}/${json.frontmatter.meta.description}</description>\n`;
+            xml += `    <title${json.frontmatter.meta.title}</title>\n`;
+            xml += `    <description>${json.frontmatter.meta.description}</description>\n`;
+
             xml += `    <link>${rootUrl}/${json.slug}</link>\n`;
             xml += `    <guid>${rootUrl}/${json.slug}</guid>\n`;
             xml += `    <pubDate>${new Date(json.frontmatter.meta.dateCreated).toUTCString()}</pubDate>\n`;
