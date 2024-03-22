e19ded0a394776dc31c54dadd38cf4cf7dc4cea1
David Johnston <david@blacksheepcode.com>
2024-03-22 15:49:17 +1100

    Fix aside box shadow.

--- app/components/Aside/Aside.tsx
+++ app/components/Aside/Aside.tsx
@@ -7,6 +7,9 @@ export function Aside(props: AsideProps) {
 
 
     return <figure className="aside">
+        <div className="triangle"></div>
+        <div className="triangle-drop-shadow"></div>
+
         {props.children}
     </figure>
 }
