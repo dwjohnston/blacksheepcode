185099e307d9a9a0d8fe99f4be118e2949b74eeb
David Johnston <david@blacksheepcode.com>
2024-03-22 21:01:55 +1100

    Image barrel files (#107)
    * Add start of the post.
    
    * Tidy Posts.tsx
    
    * All current changes.
    
    * Post basically finished.
    
    * Add note about automated dates.
    
    * Fix aside box shadow.
    
    * Allow images to come through.
    
    * Update build script.

---
+++ utils/generateImageBarrelFiles.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from "path";
+import imageSize from "image-size";
+
+
+const imageExtensions = [".webp", ".png", ".jpg", ".gif", ".jpeg"]; 
+
+export function generateImageBarrelFiles(locationOfImages ="app/assets", locationOfBarrelFile="app/generated/images.js"){
+    const files = fs.readdirSync(locationOfImages);
+    const lines = files.reduce((acc, cur) => {
+        if (imageExtensions.includes(path.extname(cur))){
+
+            const dimensions = imageSize(`${locationOfImages}/${cur}`); 
+
+            const imageName = path.basename(cur, path.extname(cur))
+
+
+            acc = `import ${imageName}_image from "${locationOfImages}/${cur}";\n` + acc; 
+
+            acc += `export const ${imageName} = {
+   str: ${imageName}_image, 
+   width: ${dimensions.width}, 
+   height: ${dimensions.height}
+}\n`
+        }
+        else {
+            // do nothing
+        }
+
+        return acc;
+    }, ''); 
+    fs.writeFileSync(locationOfBarrelFile, lines);
+    
+
+}
+
+generateImageBarrelFiles();
