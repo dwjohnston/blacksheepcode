5e0cab6f137618953640b9ade0528564b7d0e9aa
David Johnston <david@blacksheepcode.com>
2024-07-11 14:59:58 +1000

    Updates to tests, gitignore

--- cypress/e2e/realposttests.cy.ts
+++ cypress/e2e/realposttests.cy.ts
@@ -7,8 +7,7 @@ Cypress.on('uncaught:exception', (err) => {
     if (
       err.message.includes('Hydration failed because the initial UI does not match what was rendered on the server.') ||
       err.message.includes("There was an error while hydrating. Because the error happened outside of a Suspense boundary, the entire root will switch to client rendering.")
-      || err.message.includes("Minified React error #418;")
-      || err.message.includes("Minified React error #423;")
+      || err.message.includes("Minified React error")
     ) {
       return false;
     }
