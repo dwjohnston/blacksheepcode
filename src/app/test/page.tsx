94a82d62e4974ad5b23d7f65887d2166ce90121e
David Johnston <david@blacksheepcode.com>
2024-08-19 18:30:11 +1000

    Add post about publishing React packages using NextJS

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,6 @@
 import { getAllPostFrontmatter, mergeFrontmatterAndDefaultMetadata } from "@/utils/blogPosts";
 import { ListOfArticles } from "@/components/ListOfArticles/ListOfArticles";
 import { notFound } from "next/navigation";
-import {GithubPermalinkRsc} from "react-github-permalink/dist/rsc";
 
 async function getAllArticles() {
     return getAllPostFrontmatter("test")
@@ -15,8 +14,6 @@ export default async function PageLayout() {
     return <div>
 
         Test Posts
-<GithubPermalinkRsc className="clear-right" permalink="https://github.com/dwjohnston/my-private-repo/blob/main/file.md?plain=1#L1-L3"/>
-
         <ListOfArticles allFrontmatter={articles} />
     </div>
 }
