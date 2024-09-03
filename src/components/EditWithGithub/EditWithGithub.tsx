133ec50d13ac6857f77b469582099922ef61c488
David Johnston <david@blacksheepcode.com>
2024-09-03 15:30:54 +1000

    Fixes the EditWithGithub

--- src/components/EditWithGithub/EditWithGithub.tsx
+++ src/components/EditWithGithub/EditWithGithub.tsx
@@ -16,7 +16,7 @@ export const EditWithGithub = (props: EditWithGithubProps) => {
         <div className ="edit-with-github">
             <hr />
             <p>
-                Spotted an error? <a href={`https://github.com/dwjohnston/blacksheepcode/tree/master/app/routes${postName}.mdx`}>Edit this page with Github</a>
+                Spotted an error? <a href={`https://github.com/dwjohnston/blacksheepcode/tree/master/src/routes${postName}.mdx`}>Edit this page with Github</a>
             </p>
         </div>
     );
