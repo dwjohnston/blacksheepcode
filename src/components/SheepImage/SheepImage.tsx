07a5eaaafa383f56c5b51eccc44d689c62e38458
David Johnston <david@blacksheepcode.com>
2024-09-11 08:25:36 +1000

    Update behaviour of sheep images.

--- src/components/SheepImage/SheepImage.tsx
+++ src/components/SheepImage/SheepImage.tsx
@@ -2,36 +2,9 @@
 import dark from "../../assets/bsc_dark.webp";
 import light from "../../assets/bsc_light.webp";
 
-
-import { useMediaQuery } from "react-responsive";
-import { useEffect, useState } from "react";
-
-export function SheepImage(props: { mode?: "dark" | "light" }) {
-
-    const isDarkMode = useMediaQuery({ query: "(prefers-color-scheme: dark)" }) 
-
-
-    let imageToUse = isDarkMode ? dark : light; 
-
-    // Allow override via prop
-    if(props.mode){
-        if (props.mode === "dark") {
-            imageToUse = dark; 
-        }
-        if (props.mode === "light") {
-            imageToUse = light; 
-        }
-    }
-
-    // Because SSR doesn't know what the preferred theme is 
-    // We hide the image at first so you don't get the wrong one
-    const [ready, setReady] = useState(false);
-
-    useEffect(() => {
-        setReady(true);
-    }, [])
-
+export function SheepImage() {
     return <div className="sheep-image-wrapper">
-        {ready && <img src={imageToUse.src} alt="A black sheep typing at a computer" />}
+       <img src={dark.src} className="dark" alt="A black sheep typing at a computer" />
+       <img src={light.src} className="light" alt="A black sheep typing at a computer" />
     </div>
 }
