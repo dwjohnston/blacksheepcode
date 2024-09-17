8fc3914515be265e55b13823be5e97b4edfb0206
David Johnston <david@blacksheepcode.com>
2024-09-17 17:17:55 +1000

    Tidyup post comments

--- src/components/PostComments/Comment.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-const Comment = React.forwardRef<HTMLDivElement>((props, commentBox) => {
-  return <div ref={commentBox} className="comments" id="comments" tabIndex={0} />
-});
-
-Comment.displayName = "Comment";
-export default Comment;
