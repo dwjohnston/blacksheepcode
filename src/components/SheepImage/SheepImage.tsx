347c0f5b6178a479053c0efb50277a06a9383a3f
David Johnston <david@blacksheepcode.com>
2024-09-24 15:58:46 +1000

    Change some to optimised Image

--- src/components/SheepImage/SheepImage.tsx
+++ src/components/SheepImage/SheepImage.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image";
 import dark from "../../assets/bsc_dark.webp";
 import light from "../../assets/bsc_light.webp";
 
@@ -8,18 +9,18 @@ export function SheepImage(props: { mode?: "dark" | "light" }) {
     // If we have passed light/dark mode, then we force show that image without the className that will show/hide it
     if (props.mode === "dark") {
         return <div className={`sheep-image-wrapper force-dark`}>
-            <img src={dark.src} alt="A black sheep typing at a computer" />
+            <Image src={dark} alt="A black sheep typing at a computer" />
         </div>
     }
 
     if (props.mode === "light") {
         return <div className={`sheep-image-wrapper force-light`}>
-            <img src={light.src} alt="A black sheep typing at a computer" />
+            <Image src={light} alt="A black sheep typing at a computer" />
         </div>
     }
 
     return <div className={`sheep-image-wrapper`}>
-        <img src={dark.src} className="dark" alt="A black sheep typing at a computer" />
-        <img src={light.src} className="light" alt="A black sheep typing at a computer" />
+        <Image src={dark} className="dark" alt="A black sheep typing at a computer" />
+        <Image src={light} className="light" alt="A black sheep typing at a computer"  />
     </div>
 }
