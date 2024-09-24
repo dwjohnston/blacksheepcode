729fe9941ce64d3c5d83b006d112c8f5ccc357c6
David Johnston <david@blacksheepcode.com>
2024-09-24 16:23:23 +1000

    Natural state of software (#135)
    * Start on post
    
    * Update post.
    
    * Add content.
    
    * Fix images.
    
    * Change some to optimised Image
    
    * Move to posts. Update tile.

--- src/components/SheepImage/SheepImage.tsx
+++ src/components/SheepImage/SheepImage.tsx
@@ -1,10 +1,26 @@
 "use client"
+import Image from "next/image";
 import dark from "../../assets/bsc_dark.webp";
 import light from "../../assets/bsc_light.webp";
 
-export function SheepImage() {
-    return <div className="sheep-image-wrapper">
-       <img src={dark.src} className="dark" alt="A black sheep typing at a computer" />
-       <img src={light.src} className="light" alt="A black sheep typing at a computer" />
+export function SheepImage(props: { mode?: "dark" | "light" }) {
+
+
+    // If we have passed light/dark mode, then we force show that image without the className that will show/hide it
+    if (props.mode === "dark") {
+        return <div className={`sheep-image-wrapper force-dark`}>
+            <Image src={dark} alt="A black sheep typing at a computer" />
+        </div>
+    }
+
+    if (props.mode === "light") {
+        return <div className={`sheep-image-wrapper force-light`}>
+            <Image src={light} alt="A black sheep typing at a computer" />
+        </div>
+    }
+
+    return <div className={`sheep-image-wrapper`}>
+        <Image src={dark} className="dark" alt="A black sheep typing at a computer" />
+        <Image src={light} className="light" alt="A black sheep typing at a computer"  />
     </div>
 }
