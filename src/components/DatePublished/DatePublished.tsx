cbf992c17428ec2afabb8562c11e6b0d1ec2f75a
David Johnston <david@blacksheepcode.com>
2024-10-21 16:39:01 +1100

    Add space back in.

--- src/components/DatePublished/DatePublished.tsx
+++ src/components/DatePublished/DatePublished.tsx
@@ -4,7 +4,7 @@ export function DatePublished(props: { date: string | Date, className?: string})
     const {className = '', date} = props;
 
     return <div className={`date-published ${className}`}>
-        <i><span>Published:</span><time dateTime={new Date(date).toISOString()}>{new Date(date).toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' })}</time></i>
+        <i><span>Published: </span><time dateTime={new Date(date).toISOString()}>{new Date(date).toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' })}</time></i>
     </div>
 
 }
