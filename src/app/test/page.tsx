e3463b13e90b36cde5e03e059419b1bcb7442338
David Johnston <david@blacksheepcode.com>
2024-11-20 17:43:10 +1100

    React bits imperative hook (#159)
    * Implement hook
    
    * Add post
    
    * Updates
    
    * Fix implementation.
    
    * Update link
    
    * Remove advice for unemployted.

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,6 +3,7 @@ import { ListOfArticles } from "@/components/ListOfArticles/ListOfArticles";
 import { notFound } from "next/navigation";
 import { GithubPermalinkRsc } from "react-github-permalink/dist/rsc";
 import { CodeExampleLink } from "@/components/CodeExampleLink/CodeExampleLink";
+import { MyForm } from "@/demos/imperative_confirmation_modal/components/MyForm";
 
 async function getAllArticles() {
     return getAllPostFrontmatter("test")
@@ -17,7 +18,7 @@ export default async function PageLayout() {
 
         Test Posts
         <ListOfArticles allFrontmatter={articles} />
-
+    <MyForm/>
 <CodeExampleLink link="foo"/>
         <GithubPermalinkRsc permalink="https://github.com/dwjohnston/bundle-size-investigation/blob/37f8511bb56a556b1abd2c87226847e2ef30e07f/scenario-1-images-only/index.html#L1-L31"
         
