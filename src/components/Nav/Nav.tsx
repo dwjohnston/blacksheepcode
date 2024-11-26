9ceb19759c0ce70c649a2f80513c8ef3ba629b19
David Johnston <david@blacksheepcode.com>
2024-11-27 10:20:02 +1100

    Updates to home page

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { usePathname } from 'next/navigation'
+import { PropsWithChildren } from "react";
+
+ 
+const PATHS = {
+    "/posts": "Blog", 
+    "/about": "About", 
+    "/hire-me": "Hire Me"
+}
+
+export function Nav() {
+
+    const pathName = usePathname();
+
+    return <nav>
+            {Object.entries(PATHS).map((v) => {
+                const [key,value] =v; 
+                return <a href={key} key={key} className={key===pathName? "active": "not-active"}>{value}</a>
+            })}
+    </nav>
+}
