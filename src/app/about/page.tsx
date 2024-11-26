4025cd1c0c2c960f5ee140d13ac7ac5156071280
David Johnston <david@blacksheepcode.com>
2024-11-27 10:20:02 +1100

    Updates to home page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,32 @@
+export default function Page() {
+    return <div>
+
+
+        <p>David Johnston is a web developer based in Melbourne, Australia.</p>
+
+        <p>Originally from New Zealand, he moved to Melbourne in 2018, where he lives with his wife and their child.</p>
+
+        <p>His interests outside of computers include typewriters, board games, and pinball.</p>
+
+        <b>Favourite board games:</b>
+        <ul>
+            <li>Agricola</li>
+            <li>Terra Mystica</li>
+            <li>Patchwork</li>
+        </ul>
+
+        <b>Favourite Typewriter</b>
+        <ul>
+            <li>IBM Selectric</li>
+
+        </ul>    
+
+        <b>Favourite Pinball</b>
+
+        <p>
+          Hard to say. As of 2024 David owns a 1993 Data East Jurassic Park, which has him learning how to solder and use a multimeter.
+        </p>
+        
+
+    </div>
+}
