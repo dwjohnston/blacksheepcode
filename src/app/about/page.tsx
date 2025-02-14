0b02fb6b117a95621442ac0884fe1380aac0bd2a
David Johnston <david@blacksheepcode.com>
2025-02-14 15:38:09 +1100

    About page, updates to home page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,32 +1,46 @@
 export default function Page() {
     return <div>
 
+        <h1>Who am I?</h1>
 
-        <p>David Johnston is a web developer based in Melbourne, Australia.</p>
 
-        <p>Originally from New Zealand, he moved to Melbourne in 2018, where he lives with his wife and their child.</p>
+        <p>I am David Johnston, a web developer based in Melbourne, Australia.</p>
 
-        <p>His interests outside of computers include typewriters, board games, and pinball.</p>
+        <p>The core of my career as been working with JavaScript/TypeScript, Node and React, and associated tooling (bundlers, compilers, build pipelines etc).</p>
 
-        <b>Favourite board games:</b>
+        <p>I spend a lot of of time thinking how to put the patterns in place such that developers can be effective, maintain momentum and enjoy their jobs.</p>
+
+        <p>I am regular presenter at <a href="https://melbjs.com/">MelbJS.</a></p>
+
+
+        <h2>Outside of code</h2>
+
+        <p>My interests outside of computers include typewriters, board games, and pinball.</p>
+
+        <b>Favourite board games</b>
         <ul>
             <li>Agricola</li>
             <li>Terra Mystica</li>
+            <li>War of the Ring</li>
+            <li>Twilight Struggle</li>
             <li>Patchwork</li>
         </ul>
 
         <b>Favourite Typewriter</b>
         <ul>
             <li>IBM Selectric</li>
-
-        </ul>    
+        </ul>
 
         <b>Favourite Pinball</b>
 
-        <p>
-          Hard to say. As of 2024 David owns a 1993 Data East Jurassic Park, which has him learning how to solder and use a multimeter.
-        </p>
-        
+        <ul>
+            <li>            Hard to say. As of 2024 I own 1993 Data East Jurassic Park, which has me learning how to solder and use a multimeter.
+            </li>
+        </ul>
 
-    </div>
+        <h2>Contact</h2>
+
+        <a href="mailto:david@blacksheepcode.com">david@blacksheepcode.com</a>
+
+    </div >
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,11 +2,10 @@
 import { usePathname } from 'next/navigation'
 import { PropsWithChildren } from "react";
 
- 
+
 const PATHS = {
-    "/posts": "Blog", 
-    "/about": "About", 
-    "/hire-me": "Hire Me"
+    "/posts": "Blog",
+    "/about": "About",
 }
 
 export function Nav() {
@@ -14,9 +13,9 @@ export function Nav() {
     const pathName = usePathname();
 
     return <nav>
-            {Object.entries(PATHS).map((v) => {
-                const [key,value] =v; 
-                return <a href={key} key={key} className={key===pathName? "active": "not-active"}>{value}</a>
-            })}
+        {Object.entries(PATHS).map((v) => {
+            const [key, value] = v;
+            return <a href={key} key={key} className={key === pathName ? "active" : "not-active"}>{value}</a>
+        })}
     </nav>
 }
