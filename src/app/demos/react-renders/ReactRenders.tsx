dfa11dbff8e43e86352936240eed30f6554ab057
David Johnston <david@blacksheepcode.com>
2025-05-08 22:29:42 +1000

    Everything

---
+++ src/app/demos/react-renders/ReactRenders.tsx
@@ -0,0 +1,84 @@
+"use client"
+import React, { useContext } from "react";
+import "./style.css";
+
+type MyContextType = {
+    value: string;
+    setValue: (value: string) => void;
+}
+
+const MyContext = React.createContext<MyContextType>({
+    value: "foo",
+    setValue: () => {
+        throw new Error("setValue not implemented");
+    }
+});
+
+
+const MyProvider = ({ children }: { children: React.ReactNode }) => {
+    const [value, setValue] = React.useState("foo");
+    const contextValue: MyContextType = { value, setValue };
+    return <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>;
+};
+
+
+export function MyRenderTrackerDemo() {
+    const [value, setValue] = React.useState("foo");
+
+    return <MyProvider>
+
+        <button onClick={() => {
+            setValue(`${Math.random()}`);
+        }}> Cause a render of the whole application</button>
+
+        <div className="render-tracker-demo">
+
+            <StateChanger />
+            <StateDisplayer />
+
+            <SomeUnrelatedComponent />
+            <SomeUnrelatedComponent />
+            <SomeUnrelatedComponent />
+        </div>
+    </MyProvider >
+}
+
+
+function RenderTracker() {
+
+    let randX = Math.floor(Math.random() * 100);
+    let randY = Math.floor(Math.random() * 100);
+
+
+    return <div className="render-tracker">
+        <strong>Render Tracker</strong>
+        <div className="render-tracking-dot" style={{ top: randY, left: randX }}>
+        </div>
+    </div>
+}
+
+function StateChanger() {
+    const { setValue } = useContext(MyContext);
+    return <div className="state-changer">
+        <strong>State Changer</strong>
+
+        <button onClick={() => setValue(`${Math.random()}`)}>Change State</button>
+        <RenderTracker />
+    </div >
+}
+
+function StateDisplayer() {
+    const { value } = useContext(MyContext);
+    return <div className="state-displayer">
+        <strong>State Displayer</strong>
+        <div>{value}</div>
+        <RenderTracker />
+    </div>
+}
+
+function SomeUnrelatedComponent() {
+    return <div className="some-unrelated-component">
+        <strong>Some unrelated component</strong>
+        <RenderTracker />
+    </div>
+}
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,24 +4,26 @@ import { notFound } from "next/navigation";
 import { GithubPermalinkRsc } from "react-github-permalink/dist/rsc";
 import { CodeExampleLink } from "@/components/CodeExampleLink/CodeExampleLink";
 import { MyForm } from "@/demos/imperative_confirmation_modal/components/MyForm";
+import { MyRenderTrackerDemo } from "@/app/demos/react-renders/ReactRenders";
 
 async function getAllArticles() {
     return getAllPostFrontmatter("test")
 }
 
 export default async function PageLayout() {
-    if(process.env.SHOW_TEST_PAGES !== "true") {
+    if (process.env.SHOW_TEST_PAGES !== "true") {
         notFound();
     }
     const articles = await getAllArticles();
     return <div>
 
         Test Posts
+        <MyRenderTrackerDemo />
         <ListOfArticles allFrontmatter={articles} />
-    <MyForm/>
-<CodeExampleLink link="foo"/>
+        <MyForm />
+        <CodeExampleLink link="foo" />
         <GithubPermalinkRsc permalink="https://github.com/dwjohnston/bundle-size-investigation/blob/37f8511bb56a556b1abd2c87226847e2ef30e07f/scenario-1-images-only/index.html#L1-L31"
-        
-        excludeLines={[[4,5]]}/>
+
+            excludeLines={[[4, 5]]} />
     </div>
 }
