04203a9d0a6ebf84ec04ce129a7fe22f3be582ae
David Johnston <david@blacksheepcode.com>
2025-05-09 18:15:56 +1000

    Update permalinks

--- src/demos/react-renders/common.tsx
+++ src/demos/react-renders/common.tsx
@@ -12,28 +12,3 @@ export function RenderTracker() {
     </div >
 }
 
-export function StateChanger() {
-    const { setValue } = useContext(MyContext);
-    return <div className="state-changer">
-        <strong>State Changer</strong>
-
-        <button onClick={() => setValue(`${Math.random()}`)}>Change State</button>
-        <RenderTracker />
-    </div >
-}
-
-export function StateDisplayer() {
-    const { value } = useContext(MyContext);
-    return <div className="state-displayer">
-        <strong>State Displayer</strong>
-        <div>{value}</div>
-        <RenderTracker />
-    </div>
-}
-
-function SomeUnrelatedComponent() {
-    return <div className="some-unrelated-component">
-        <strong>Some unrelated component</strong>
-        <RenderTracker />
-    </div>
-}
