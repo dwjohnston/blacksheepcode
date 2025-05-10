4c01796ffd4be146baa35afe32eb8a524f747a0e
David Johnston <david@blacksheepcode.com>
2025-05-10 19:08:44 +1000

    Updates to react rendering blog post (#205)
    * Updates to blog post
    
    * Update the permalink
    
    * Update read me
    
    * Add info panel to TIL post

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,11 @@
+import { ReactRenders1, ReactRenders2, ReactRenders3, ReactRenders4 } from "@/demos/react-renders";
+
+export default function SandBox() {
+    return <div>
+        <p>
+            This is a sandbox page.
+        </p>
+
+        <ReactRenders1 />
+    </div>
+}
--- src/components/InfoPanel/InfoPanel.tsx
+++ src/components/InfoPanel/InfoPanel.tsx
@@ -1,20 +1,22 @@
 import type { PropsWithChildren } from "react";
 
 type InfoPanelProps = PropsWithChildren<{
-    level: "warning" | "info" | "instruction"; 
-    className?: string; 
+    level: "warning" | "info" | "instruction";
+    className?: string;
 }>
 
-export function InfoPanel(props: InfoPanelProps){
+export function InfoPanel(props: InfoPanelProps) {
 
-    const {level, className ='', children} = props; 
+    const { level, className = '', children } = props;
 
-    return <div className ={`infopanel infopanel-${level} ${className}`}> 
+    return <div className={`infopanel infopanel-${level} ${className}`}>
         {level === "warning" && <span className="icon material-symbols-outlined">warning</span>}
-        {level === "info" && <span className ="icon material-symbols-outlined">info</span>}
-        {level === "instruction" && <span className ="icon material-symbols-outlined">lightbulb</span>}
+        {level === "info" && <span className="icon material-symbols-outlined">info</span>}
+        {level === "instruction" && <span className="icon material-symbols-outlined">lightbulb</span>}
 
-        {children}
+        <div className="infopanel-content">
+            {children}
+        </div>
     </div>
 
 }
