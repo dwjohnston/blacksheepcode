22592b9a45e01332dcc1fbf4b7e89a0beed44e64
David Johnston <david@blacksheepcode.com>
2025-10-23 09:48:10 +1100

    Update posts

--- src/demos/encapsulate-state/SpecialButtonDemo.tsx
+++ src/demos/encapsulate-state/SpecialButtonDemo.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { SpecialButton2 } from "./SpecialButton2";
 
-export function EncapsulateStateDemo() {
+export function SpecialButtonDemo() {
     return <SpecialButton2 onClick={async () => {
         await new Promise((res) => setTimeout(res, 1000));
         return { success: true };
